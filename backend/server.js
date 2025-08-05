39f2ee1ccc1b8b77153ad7cdd01dc73274006f95
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-05 19:55:40 +0530

    server implementation

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./config/db.js"
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("API is running...")
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`)
+})
