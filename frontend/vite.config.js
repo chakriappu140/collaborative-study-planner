60e266f284673b7b8cf7a648f03eb7fc595524fa
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-11 02:32:33 +0530

    fix4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite"
+    import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [
-        react(),
-        tailwindcss()
-    ],
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
+    export default defineConfig({
+      plugins: [react()],
+      root: './',
+      build: {
+        outDir: 'dist',
+        rollupOptions: {
+          input: {
+            main: './index.html'
+          }
+        }
       }
-    }
-  }
-});
+    });
+    
