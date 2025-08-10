825062abbfcd756740604f2acb0145c028237b96
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-11 02:39:41 +0530

    fix6

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from "react-dom/client"
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {SocketProvider} from "./context/SocketContext.jsx"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './tailwind.css'; // Change this import to the new CSS file
+import { SocketProvider } from './context/SocketContext.jsx'; 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SocketProvider>
-      <App/>
+      <App />
     </SocketProvider>
   </React.StrictMode>
-)
+);
