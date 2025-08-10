dc467f9647ce16a3a795926a648bb379133f4198
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-11 03:05:12 +0530

    fix16

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,11 @@
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
-    // import tailwindcss from 'tailwindcss';
-    // import autoprefixer from 'autoprefixer';
-    import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-    export default defineConfig({
-      plugins: [react(), tailwindcss()],
-      // css: {
-      //   postcss: {
-      //     plugins: [
-      //       tailwindcss,
-      //       autoprefixer,
-      //     ],
-      //   },
-      // },
-      root: './',
-      build: {
-        outDir: 'dist',
-        rollupOptions: {
-          input: {
-            main: './index.html'
-          }
-        }
-      }
-    });
-    
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
