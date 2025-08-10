0fe2a89d0203cc5e9313d0af3eb43e8a6b856b4a
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-11 02:51:16 +0530

    fix10

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-    import React from 'react';
-    import ReactDOM from 'react-dom/client';
-    import App from './App.jsx';
-    import './index.css'; // Change this import to the correct file
-    import { SocketProvider } from './context/SocketContext.jsx'; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Import the main CSS file here
+import { SocketProvider } from './context/SocketContext.jsx'; 
 
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <SocketProvider>
-          <App />
-        </SocketProvider>
-      </React.StrictMode>
-    );
-    
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SocketProvider>
+      <App />
+    </SocketProvider>
+  </React.StrictMode>
+);
