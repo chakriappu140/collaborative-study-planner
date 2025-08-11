1fc3d8d1888d40fda8209f0dd56391b4fdc50111
chakriappu140 <chakravarthyanekula@gmail.com>
2025-08-11 19:00:14 +0530

    authprovider1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import {AuthProvider} from "../context/AuthContext.jsx"
+import {AuthProvider} from "./context/AuthContext.jsx"
 import { SocketProvider } from './context/SocketContext.jsx'; // Import the provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
